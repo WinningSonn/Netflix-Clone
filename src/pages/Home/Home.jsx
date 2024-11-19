1f138963527670137b2cf803e9ef06641cefe225
Milson Feliciano <milson0403@gmail.com>
2024-11-19 11:11:07 +0700

    Progress
    Initial Progress

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import NarBar from '../../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div className='home' >
+      <NarBar/>
+    </div>
+  )
+}
+
+export default Home
